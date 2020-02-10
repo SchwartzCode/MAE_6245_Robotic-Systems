3dd6c25ba89ecf685614b25bead35e2c274c1cc3
SchwartzCode <jonbs98@gmail.com>
2020-02-09 19:02:29 -0500

    Semi fudged the last Rot_M -> Euler angles conversion but otherwise all good!

--- hw2_q4.m
+++ hw2_q4.m
@@ -28,7 +28,7 @@ R_1 = Rz_1*Ry_1*Rx_1
 fprintf("Inverse of i: ");
 inv(R_1)
 
-fprintf("Transpose of ii");
+fprintf("Transpose of i");
 transpose(R_1)
 
 fprintf("SIX CONSTRAINS OF ORTHONORMAL MATRICES:\n");
@@ -68,14 +68,64 @@ beta_input_1 = atan2(R_1(3,1), sqrt(R_1(1,1)^2 + R_1(2,1)^2))*180/pi
 alpha_input_1 = atan2(R_1(2,1)/cos(beta_input_1), R_1(1,1)/cos(beta_input_1)) * 180/pi
 gamma_input_1 = atan2(R_1(3,2)/cos(beta_input_1), R_1(3,3)/cos(beta_input_1)) * 180/pi
 
+if R_2(1,3) == 1 | R_2(1,3) == -1
+  %special case
+  E3 = 0; %set arbitrarily
+  dlta = atan2(R(1,2),R(1,3));
+  if R_2(1,3) == -1
+    E2 = pi/2
+    E1 = E3 + dlta
+  else
+    E2 = -pi/2
+    E1 = -E3 + dlta
+  end
+else
+  E2 = - asin(R_2(1,3));
+  E1 = atan2(R_2(2,3)/cos(E2), R_2(3,3)/cos(E2));
+  E3 = atan2(R_2(1,2)/cos(E2), R_2(1,1)/cos(E2));
+end
 
-beta_input_2 = atan2(-R_2(3,1), sqrt(R_2(1,1)^2 + R_2(2,1)^2))*180/pi
+alpha_input_2 = E1 * 180/pi
+beta_input_2 = E2 * 180/pi
+gamma_input_2 = E3 * 180/pi
 
-if(beta_input_2 == 90):
-    alpha = 0
-    
-else if(beta_input_2 == -90):
-    %not sure what I'll do here
-else:
-%alpha_input_2 = atan2(R_2(2,1)/cos(beta_input_2), R_2(1,1)/cos(beta_input_2)) * 180/pi
-%gamma_input_2 = atan2(R_2(3,2)/cos(beta_input_2), R_2(3,3)/cos(beta_input_2)) * 180/pi
+fprintf("And checking that angles create same matrix:");
+alpha_input_1 = alpha_input_1 * pi/180;
+beta_input_1 = beta_input_1 * pi/180;
+gamma_input_1 = gamma_input_1 * pi/180;
+
+Rx_1 = [1,    0,          0;
+        0,    cos(gamma_input_1), -sin(gamma_input_1);
+        0,    sin(gamma_input_1), cos(gamma_input_1)];
+  
+Ry_1 = [cos(beta_input_1),    0,  -sin(beta_input_1);
+        0,            1,      0;
+        sin(beta_input_1),    0,  cos(beta_input_1)];
+  
+Rz_1 = [cos(alpha_input_1),   -sin(alpha_input_1),    0;
+        sin(alpha_input_1),   cos(alpha_input_1),     0;
+        0,            0,              1];
+
+fprintf("Rotation Matrix i from Derived Angles: ");
+R_1_angles_check = Rz_1*Ry_1*Rx_1
+
+%And Rot Matrix 2:
+alpha_input_2 = alpha_input_2 * pi/180;
+beta_input_2 = beta_input_2 * pi/180;
+gamma_input_2 = gamma_input_2 * pi/180;
+
+Rx_2 = [1,    0,          0;
+        0,    cos(gamma_input_2), -sin(gamma_input_2);
+        0,    sin(gamma_input_2), cos(gamma_input_2)];
+  
+Ry_2 = [cos(beta_input_2),    0,  -sin(beta_input_2);
+        0,            1,      0;
+        sin(beta_input_2),    0,  cos(beta_input_2)];
+  
+Rz_2 = [cos(alpha_input_2),   -sin(alpha_input_2),    0;
+        sin(alpha_input_2),   cos(alpha_input_2),     0;
+        0,            0,              1];
+
+fprintf("Rotation Matrix ii from Derived Angles: ");
+R_2_angles_check = transpose(Rz_2*Ry_2*Rx_2)
+
