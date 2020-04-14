19714d4a4c4aa1f4375c6e9b31239248806ad4a4
SchwartzCode <jonbs98@gmail.com>
2020-04-14 18:48:46 -0400

    Removing old locations of files

--- hw5/inverseEqn2.m
+++
@@ -1,9 +0,0 @@
-% Function to Calculate Residual
-function [f] = inverseEqn2(theta1)
-    x = 20/sqrt(2);
-    y = 20/sqrt(2);
-    a1 = 10;
-    a2 = 10;
-
-    f = -(a1*sin(theta1) + a2*sin(acos((x - a1*cos(theta1))/a2)) - y);
-end
